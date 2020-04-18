de30a59344badb399813228471c8ff0586035aa6
Francesca Sargent <francesca.sargent@admiralgroup.co.uk>
2020-04-18 22:40:10 +0100

    Add sound

--- __init__.py
+++ __init__.py
@@ -1,13 +1,24 @@
 from mycroft import MycroftSkill, intent_file_handler
+from mycroft.skills.audioservice import AudioService
+import os
 
 
 class Fart(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+    
+    def initialise(self):
+        self.audio_service = AudioService(self.bus)
 
     @intent_file_handler('fart.intent')
     def handle_fart(self, message):
         typeof = message.data.get('typeof')
+
+        directory = os.path.dirname(os.path.realpath(__file__))
+
+        self.log.info(directory)
+
+        self.audio_service.play(f'file://{directory}/fart_sample.wav')
 
         self.speak_dialog('fart', data={
             'typeof': typeof
